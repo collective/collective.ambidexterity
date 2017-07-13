0dc2c656f53d0a15d8563a716481d226c85c40b5
Steve McMahon <steve@dcn.org>
2017-07-13 14:50:30 -0700

    consolidate test modules; rough out dotted validator

--- src/collective/ambidexterity/tests/testBasics.py
+++ src/collective/ambidexterity/tests/testBasics.py
@@ -2,14 +2,60 @@
 """Idea tests for this package."""
 
 from collective.ambidexterity.testing import COLLECTIVE_AMBIDEXTERITY_INTEGRATION_TESTING  # noqa
-from collective.ambidexterity.tests.classtest import MethodBinder
 from new import instancemethod
+from plone import api
 from plone.app.testing import applyProfile
 from plone.dexterity.utils import createContent
+from z3c.form.validator import SimpleFieldValidator
 from zope.dottedname.resolve import resolve as dottedname_resolve
+from zope.interface import Invalid
+from zope.interface import provider
 from zope.schema.interfaces import IContextAwareDefaultFactory
 
 import unittest
+
+
+class DottedValidator(SimpleFieldValidator):
+    """ SimpleFieldValidator that calls a dotted path
+        for simple validation. """
+
+    # a class property to hold the dotted path
+    dotted_path = None
+
+    def validate(self, value):
+        super(DottedValidator, self).validate(value)
+
+        result = dottedname_resolve(self.dotted_path)
+        if getattr(result, 'lower', None) is not None:
+            raise Invalid(result)
+
+
+class MethodBinder():
+
+    @provider(IContextAwareDefaultFactory)
+    def default(self, context):
+        return 42
+
+    @provider(IContextAwareDefaultFactory)
+    def default2(self, context):
+        return context.title + ' is 42'
+
+    @provider(IContextAwareDefaultFactory)
+    def default3(self, context):
+        portal_resources = api.portal.get_tool(name='portal_resources')
+        return portal_resources.test_me(context)
+
+
+class SimpleClass():
+    pass
+
+
+method_binder_object = MethodBinder()
+
+test_obj = SimpleClass()
+test_obj.sample1 = MethodBinder()
+test_obj.node = SimpleClass()
+test_obj.node.sample2 = MethodBinder()
 
 
 class TestSetup(unittest.TestCase):
@@ -23,15 +69,17 @@ class TestSetup(unittest.TestCase):
         self.portal = self.layer['portal']
         applyProfile(self.portal, 'collective.ambidexterity:testing')
 
+        self.test_schema = self.portal.portal_types.simple_test_type.lookupSchema()
+
         self.portal.portal_resources.manage_addProduct['PythonScripts'].manage_addPythonScript('test_me')
         script = self.portal.portal_resources.test_me
         # order important here. bindings must be cleared before we can set 'context' as a parameter.
         script.ZBindings_edit([])
         script.ZPythonScript_edit('context', 'return u"test script %s" % context.title')
 
-        self.my_object = dottedname_resolve('collective.ambidexterity.tests.classtest.method_binder_object')
-        self.my_method = dottedname_resolve('collective.ambidexterity.tests.classtest.method_binder_object.default')
-        self.my_method2 = dottedname_resolve('collective.ambidexterity.tests.classtest.method_binder_object.default2')
+        self.my_object = dottedname_resolve('collective.ambidexterity.tests.testBasics.method_binder_object')
+        self.my_method = dottedname_resolve('collective.ambidexterity.tests.testBasics.method_binder_object.default')
+        self.my_method2 = dottedname_resolve('collective.ambidexterity.tests.testBasics.method_binder_object.default2')
 
     def test_resolve(self):
         self.assertIsInstance(self.my_object, MethodBinder)
@@ -43,13 +91,13 @@ class TestSetup(unittest.TestCase):
 
     def test_dict_resolve(self):
         sample_method = dottedname_resolve(
-            'collective.ambidexterity.tests.classtest.test_obj.sample1.default')
+            'collective.ambidexterity.tests.testBasics.test_obj.sample1.default')
         self.assertTrue(IContextAwareDefaultFactory.providedBy(sample_method))
         self.assertEqual(sample_method(None), 42)
 
     def test_depth_resolve(self):
         sample_method = dottedname_resolve(
-            'collective.ambidexterity.tests.classtest.test_obj.node.sample2.default2')
+            'collective.ambidexterity.tests.testBasics.test_obj.node.sample2.default2')
         self.assertTrue(IContextAwareDefaultFactory.providedBy(sample_method))
         self.assertEqual(sample_method(self.portal), u'Plone site is 42')
 
@@ -66,9 +114,25 @@ class TestSetup(unittest.TestCase):
 
     def test_script_via_dots(self):
         sample_method = dottedname_resolve(
-            'collective.ambidexterity.tests.classtest.test_obj.sample1.default3')
+            'collective.ambidexterity.tests.testBasics.test_obj.sample1.default3')
         self.assertEqual(sample_method(self.portal), 'test script Plone site')
 
     def test_default_from_script(self):
         test_item = createContent('simple_test_type', title=u'The Meaning of Life')
         self.assertEqual(test_item.test_string_field2, u'test script The Meaning of Life')
+
+"""
+notes
+
+fti = portal_types.typename
+schema = fti.lookupSchema()
+schema.validateInvariants(something)
+field = schema.get('test_string_field')
+field.validate(something)
+
+schema.get('test_integer_field').validate('five')
+WrongType: ('five', (<type 'int'>, <type 'long'>), 'test_integer_field')
+
+schema.get('test_integer_field').validate(5)
+None
+"""
