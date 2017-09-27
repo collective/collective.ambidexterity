e52f20b325c0182b419bbcd9c0567ee4d15d366e
Steve McMahon <steve@dcn.org>
2017-09-27 15:02:55 -0700

    Back to development: 1.0b2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.ambidexterity',
-    version='1.0b1',
+    version='1.0b2.dev0',
     description="TTW support for Dexterity",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
