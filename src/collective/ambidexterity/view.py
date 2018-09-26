e47e35018fcd657fd9813438a0ad73c5c35908a8
Jon Pentland <jon@iomedia.co.uk>
2018-09-26 13:52:40 +0100

    add missing import

--- src/collective/ambidexterity/view.py
+++ src/collective/ambidexterity/view.py
@@ -12,6 +12,7 @@
 
 
 from plone import api
+from Products.Five import BrowserView
 from plone.dexterity.browser.view import DefaultView
 from Products.Five.browser.pagetemplatefile import BoundPageTemplate
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
